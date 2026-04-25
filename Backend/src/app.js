d934f0d211f0b79149f7e6238bae09dd29c30ef2
CodeWithGanesh1 <gk9605873@gmail.com>
2026-04-25 12:50:56 +0530

    fix cors issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,19 +5,24 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+// ISKO UPDATE KAREIN
 app.use(cors({
-    origin: "https://moodify-se2x.vercel.app",
+    origin: [
+        "https://moodify-se2x.vercel.app", 
+        "https://moodify-seven-zeta.vercel.app",
+        "http://localhost:5173" // Local testing ke liye bhi rakhein
+    ],
     credentials: true
-}))
+}));
 
 /**
  * Routes
  */
-
 const authRoutes = require("./routes/auth.routes")
 const songRoute = require("./routes/song.routes")
 
 app.use("/api/auth", authRoutes)
 app.use("/api/songs", songRoute)
 
-module.exports = app 
+module.exports = app;
